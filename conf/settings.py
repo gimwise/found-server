f995dd99a044db3e2f757cd1287aa38a29db0e94
gimwise <sophia1142004@gmail.com>
2022-08-19 22:11:29 +0900

    heroku

--- conf/settings.py
+++ conf/settings.py
@@ -191,9 +191,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 
 import dj_database_url
