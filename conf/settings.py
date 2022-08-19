082921cb26fa327ab9a6e961d333ad69f1d933f0
gimwise <sophia1142004@gmail.com>
2022-08-19 22:18:06 +0900

    heroku

--- conf/settings.py
+++ conf/settings.py
@@ -194,9 +194,8 @@ USE_TZ = True
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 
 
